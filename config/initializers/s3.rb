7edfd76022a719bded7fe2929bf69da1ab21ab6e
sammy1088 <szshreder@gmail.com>
2015-08-03 00:49:58 +0000

    sdf

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,7 +1,12 @@
 # initializers/s3.rb
 if Rails.env == "production"
   # set credentials from ENV hash
-  S3_CREDENTIALS = { :access_key_id => ENV['S3_KEY'], :secret_access_key => ENV['S3_SECRET'], :bucket => "foodweb"}
+  S3_CREDENTIALS = {
+    :bucket => ENV['S3_BUCKET_NAME'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+
 else
   # get credentials from YML file
   S3_CREDENTIALS = Rails.root.join("config/s3.yml")
