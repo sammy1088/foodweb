c29745e3eff54f3818f1452fc34f4a74a3d2a5c4
sammy1088 <szshreder@gmail.com>
2015-08-10 21:43:09 +0000

    sdf

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -37,6 +37,12 @@ end
 
     @post = Post.find(params[:id])
 end
+   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+ 
+    redirect_to posts_path
+  end
 def update
     @post = Post.find(params[:id])
     @post.update(post_params)
