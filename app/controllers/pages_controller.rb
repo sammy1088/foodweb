3722ebc3cd5e8e90eda9a076ae3387face20f573
sammy1088 <szshreder@gmail.com>
2015-10-21 02:06:52 +0000

    sdf

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,7 +13,34 @@ class PagesController < ApplicationController
  @hash = Gmaps4rails.build_markers(@posts) do |post, marker|
   marker.lat post.latitude
   marker.lng post.longitude
-
+    if post.user.categ == "Restaurant"
+      marker.picture({
+        :url => "http://www.google.com/mapfiles/ms/micons/restaurant.png",
+                  :width   => 60,
+                  :height  => 60
+          }) 
+    end
+    if post.user.categ == "Buyer"
+      marker.picture({
+                  :url => "http://www.google.com/mapfiles//ms/micons/shopping.png",
+                  :width   => 60,
+                  :height  => 60
+          }) 
+    end
+    if post.user.categ == "Grocery Store"
+      marker.picture({
+        :url => "http://www.google.com/mapfiles//ms/micons/grocerystore.png",
+                  :width   => 60,
+                  :height  => 60
+          }) 
+    end
+    if post.user.categ == "Seller"
+      marker.picture({
+        :url => "http://www.google.com/mapfiles//ms/micons/homegardenbusiness.png",
+                  :width   => 60,
+                  :height  => 60
+          }) 
+    end
     marker.infowindow render_to_string(:partial => "mapbox", locals: { post: post })
      
     end
