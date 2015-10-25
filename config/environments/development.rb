03c239608b167abccdcd656c8f363ab7c5c99f9e
sammy1088 <szshreder@gmail.com>
2015-10-25 23:46:44 +0000

    addmail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,17 +34,16 @@ Foodweb::Application.configure do
   config.assets.debug = true
 
   # Mailer
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: ENV["SMTP_SERVER"],
-    openssl_verify_mode: OpenSSL::SSL::VERIFY_NONE,
-    port: ENV["SMTP_PORT"].to_i,
-    domain: ENV["MAILER_DOMAIN"],
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV["SMTP_USER"],
-    password: ENV["SMTP_PWD"]
+address: "smtp.gmail.com",
+port: 587,
+domain: ENV["GMAIL_DOMAIN"],
+authentication: "plain",
+enable_starttls_auto: true,
+user_name: ENV["GMAIL_USERNAME"],
+password: ENV["GMAIL_PASSWORD"]
   }
 end
