7d1654c1bc932f7f3b2ad1e6f528654bbe969af0
sammy1088 <szshreder@gmail.com>
2015-10-26 01:06:41 +0000

    sdf

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ Foodweb::Application.configure do
 
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { :host => 'smtp.sendgrid.net' }
+  config.action_mailer.default_url_options = { :host => 'foodweb.io' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
