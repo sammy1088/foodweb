552a3cb3d4e931ca3f625014e6d50d4685ce6d37
sammy1088 <szshreder@gmail.com>
2015-10-26 00:08:15 +0000

    sdf

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,8 +88,9 @@ Foodweb::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   # Mailer
-  Rails.application.routes.default_url_options[:host] = 'foodweb.io'
+
   config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { :host => 'foodweb.io' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
