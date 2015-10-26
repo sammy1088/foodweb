bb8e38e808734d90d1217a5f8bb565c6438af564
sammy1088 <szshreder@gmail.com>
2015-10-26 00:42:06 +0000

    df

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,12 +94,12 @@ Foodweb::Application.configure do
   config.action_mailer.default_url_options = { :host => 'foodweb.io' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-address: "smtp.gmail.com",
-port: 587,
-domain: ENV["GMAIL_DOMAIN"],
-authentication: "plain",
-enable_starttls_auto: true,
-user_name: ENV["GMAIL_USERNAME"],
-password: ENV["GMAIL_PASSWORD"]
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
   }
 end
